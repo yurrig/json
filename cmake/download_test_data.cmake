b2244a7f5fcf3288ef3aacb17dd82813e5aff81f
Niels Lohmann <mail@nlohmann.me>
2021-01-26 21:28:15 +0100

    :checkered_flag: make code portable #2593

--- cmake/download_test_data.cmake
+++ cmake/download_test_data.cmake
@@ -8,12 +8,15 @@ if(JSON_TestDataDirectory)
     file(WRITE ${CMAKE_BINARY_DIR}/include/test_data.hpp "#define TEST_DATA_DIRECTORY \"${JSON_TestDataDirectory}\"\n")
 else()
     find_package(Git)
-    # target to download test data
-    add_custom_target(download_test_data
-        COMMAND test -d json_test_data || ${GIT_EXECUTABLE} clone -c advice.detachedHead=false --branch v${JSON_TEST_DATA_VERSION} ${JSON_TEST_DATA_URL}.git --quiet --depth 1
+    # command to download test data
+    add_custom_command(
+        OUTPUT ${CMAKE_BINARY_DIR}/json_test_data
+        COMMAND ${GIT_EXECUTABLE} clone -c advice.detachedHead=false --branch v${JSON_TEST_DATA_VERSION} ${JSON_TEST_DATA_URL}.git --quiet --depth 1
         COMMENT "Downloading test data from ${JSON_TEST_DATA_URL} (v${JSON_TEST_DATA_VERSION})"
         WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     )
+    # target to only download data once
+    add_custom_target(download_test_data DEPENDS ${CMAKE_BINARY_DIR}/json_test_data)
     # create a header with the path to the downloaded test data
     file(WRITE ${CMAKE_BINARY_DIR}/include/test_data.hpp "#define TEST_DATA_DIRECTORY \"${CMAKE_BINARY_DIR}/json_test_data\"\n")
 endif()
